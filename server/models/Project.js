c00d08e53574d4392ab795024e958d29e56d20a8
dannikaml <dannikaml@hotmail.com>
2023-05-16 18:58:07 -0500

    updated models/Project.js to account for tasks

--- server/models/Project.js
+++ server/models/Project.js
@@ -1,5 +1,20 @@
 const { Schema, model } = require('mongoose');
 
+const taskSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+});
+
 const projectSchema = new Schema({
   title: {
     type: String,
@@ -13,6 +28,7 @@ const projectSchema = new Schema({
     type: Boolean,
     default: true,
   },
+  tasks: [taskSchema], // Array of tasks associated with the project
 });
 
 const Project = model('Project', projectSchema);
