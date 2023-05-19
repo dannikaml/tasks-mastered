818db11a70ac6be606134a2c2242aa62308229e0
alexandramunn <aam528@gmail.com>
2023-05-18 19:27:11 -0500

    jwt token

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql', // Replace with your server URL
+  uri: 'http://localhost:3001/graphql', // Replace with your server URL
   cache: new InMemoryCache(),
 });
 
